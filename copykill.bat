b44745a05cd133534b77176cfe78d165558cc3e6
Raul Salitrero <raulsalitrero@gmail.com>
2021-10-15 13:12:40 -0600

    Commit inicial

---
+++ copykill.bat
@@ -0,0 +1,20 @@
+copy /y sitios.7z \\10.18.226.137\C$\users\raul.salitrero\Desktop
+call rimraf \\10.18.226.137\C$\users\raul.salitrero\Desktop\sitios
+copy /y sitios.7z \\10.18.226.171\C$\users\raul.salitrero\Desktop
+call rimraf \\10.18.226.171\C$\users\raul.salitrero\Desktop\sitios
+copy /y sitios.7z \\10.18.226.211\C$\users\raul.salitrero\Desktop
+call rimraf \\10.18.226.211\C$\users\raul.salitrero\Desktop\sitios
+copy /y sitios.7z \\10.18.226.212\C$\users\raul.salitrero\Desktop
+call rimraf \\10.18.226.212\C$\users\raul.salitrero\Desktop\sitios
+copy /y sitios.7z \\10.18.226.213\C$\users\raul.salitrero\Desktop
+call rimraf \\10.18.226.213\C$\users\raul.salitrero\Desktop\sitios
+rem copy /y sitios.7z \\10.18.226.200\C$\users\raul.salitrero\Desktop
+rem call rimraf \\10.18.226.200\C$\users\raul.salitrero\Desktop\sitios
+
+start mstsc /v:10.18.226.137 /admin
+start mstsc /v:10.18.226.171 /admin
+start mstsc /v:10.18.226.211 /admin
+start mstsc /v:10.18.226.212 /admin
+start mstsc /v:10.18.226.213 /admin
+
+rem start mstsc /v:10.18.226.200 /admin
---
+++ index.js
@@ -0,0 +1,302 @@
+/*+ jshint nodejs,es6 */
+"use strict";
+const util = require('util');
+const rimraf = util.promisify(require("rimraf"));
+const {
+    exec
+} = require("child_process");
+let sitios = require("./sitios.json");
+const yargs = require('yargs/yargs');
+const chalk = require('chalk');
+const { hideBin } = require('yargs/helpers');
+let args = yargs(hideBin(process.argv));
+const inquirer = require('inquirer');
+const pLimit = require('promise-limit');
+const fs = require('fs-extra');
+
+
+let ultimos = sitios;
+try {
+    ultimos = require("./ultconfig.json");
+   // console.log('ult', ultimos);
+    /* obtiene todas las propiedades del sitio desde sitios.json si existe y el valor de compilar del ultimos */
+    ultimos.sitios = ultimos.sitios.map(ult => {
+        let actual = sitios.sitios.find(act => act.clave === ult.clave) || ult;
+        let merged = { ...actual, compilar: ult.compilar };
+        return merged;
+    });
+} catch (e) {
+    console.error(e);
+    ultimos = sitios;
+}
+
+
+(async () => {
+    args = args.usage("node index.js [opciones]").locale("es").option('?', {
+        alias: 'h'
+    }).alias('?', 'ayuda').option('d', {
+        alias: 'desatendido',
+        demand: false,
+        type: 'boolean',
+        describe: 'Indica si no realiza preguntas (usa sitios.json)'
+    }).option('u', {
+        alias: 'ultima',
+        demand: false,
+        type: 'boolean',
+        describe: 'Repite la ultima compilacion (usa ultconfig.json, implica -d)'
+    }).help('?');
+    args.showHelp();
+    args = args.argv;
+    if (args.h) {
+        return;
+    }
+
+    const repite = args.u;
+    const desatendido = args.d || repite;
+    if (repite) {
+        sitios = ultimos;
+    }
+    const promiseFromChildProcess = function promiseFromChildProcess(child) {
+        return new Promise((resolve, reject) => {
+            child.addListener("exit", resolve);
+        });
+    };
+
+    const prompt = async function prompt(query, defa, validar = undefined) {
+        if (!desatendido) {
+            return (await inquirer.prompt([
+                {
+                    type: 'input',
+                    message: query,
+                    name: 'valor',
+                    validate: validar || undefined
+                }])).valor
+        } else {
+            return defa;
+        }
+    };
+
+    const promptSiNo = async function promptSiNo(query, defa) {
+        if (!desatendido) {
+            let v = (await inquirer.prompt([
+                {
+                    type: 'input',
+                    message: query + chalk.cyan(` (S[${chalk.underline('i')}]/N[${chalk.underline('o')}])`),
+                    name: 'valor',
+                    default: defa ? 'S' : 'N',
+                    validate: function (valor) {
+                        return /^(si?|no?)$/ig.test((valor || '').trim()) ||
+                            'Escriba un valor valido' + chalk.cyan(` (S[${chalk.underline('i')}]/N[${chalk.underline('o')}])`);
+                    }
+                }])).valor;
+            v = (v === "" ? (defa ? "S" : "N") : v);
+            return (v.toLowerCase().substr(0, 1) === "s");
+        } else {
+            return defa;
+        }
+    };
+
+    const promptHidden = async function promptHidden(query, defa, validar = undefined) {
+        if (!desatendido) {
+            return (await inquirer.prompt([
+                {
+                    type: 'password',
+                    message: query,
+                    name: 'valor',
+                    default: defa,
+                    validate: validar || undefined
+                }])).valor;
+        } else {
+            defa;
+        }
+    };
+
+    const limpias = sitios.sitios.map(async x => {
+        console.log(`limpiando ${x.folder}`);
+        let r = {
+            isFulfilled: () => !this.err,
+            reason: () => this.err && '' + (this.err?.message ?? '') + '\n'
+                (this.err?.stack ?? ''),
+            value: undefined,
+            tag: undefined
+        }
+
+        r.tag = x.folder;
+        try {
+            r.value = await rimraf(`built\\${x.folder}`);
+            return r
+        }
+        catch (ex) {
+            r.err = ex;
+        }
+        return r;
+    });
+
+    let cl = await Promise.all(limpias);
+    cl.forEach(inspection => {
+        if (!inspection.isFulfilled()) {
+            console.error(`Error Limpiando: ${inspection.tag} ${inspection.reason()}`);
+        } else {
+            console.log(`listo: ${inspection.tag}`);
+        }
+    })
+    await rimraf("sitios.7z");
+    sitios.vars.comprimir = await promptSiNo("Comprimir?", sitios.vars.comprimir ?? true);
+    sitios.vars.borrarwc = await promptSiNo("Borrar Web.config?", sitios.vars.borrarwc ?? true);
+    sitios.vars.obtener = await promptSiNo("Obtener Sources?", sitios.vars.obtener ?? true);
+    if (!desatendido) {
+        let resp = await inquirer.prompt([
+            {
+                type: 'checkbox',
+                message: 'Sitios a Compilar',
+                name: 'compilar',
+                choices: sitios.sitios.map(s => ({
+                    name: s.clave,
+                    checked: s.compilar
+                })),
+                pageSize: 20
+            },
+        ]);
+        sitios.sitios.filter(s => {
+            return resp.compilar.includes(s.clave);
+        }).forEach(s => s.compilar = true);
+    }
+    if (sitios.vars.comprimir) {
+        let pw = await promptHidden("Contraseña deseada (vacio para ninguna)", sitios.vars.pw7 || "");
+        let pw2 = await promptHidden("Repetir Contraseña deseada (vacio para ninguna)", sitios.vars.pw7 || "");
+        if (pw !== pw2) {
+            console.log("los Passwords no coinciden, se dejara vacio");
+            sitios.vars.pw7 = "";
+        } else {
+            sitios.vars.pw7 = pw;
+        }
+    } else {
+        sitios.vars.pw7 = sitios.vars.pw7 || "";
+    } try {
+        await fs.outputJSON('ultconfig.json', sitios);
+        console.log("listo grabando ultconfig.json");
+    }
+    catch (ex) {
+        console.error(`Error grabando ultconfig.json: ${ex.message}`);
+    }
+
+    const publicados = sitios.sitios.filter(s => s.compilar).map(s => s.folder + (s.keyVersion ?
+        ':' + s.keyVersion : ''));
+    try {
+        await fs.outputJSON('publicados.json', publicados);
+        console.log("listo grabando publicados.json");
+    } catch (ex) {
+        console.error(`Error grabando publicados.json: ${inspection.reason()}`);
+    }
+
+    let promesasSVN = [];
+    if (sitios.vars.obtener) {
+        promesasSVN = sitios.sitios.map(x => {
+            if (x.compilar) {
+                const child = exec(`svn checkout ${x.repo} svn\\${x.folder}`);
+                child.stdout.on('data', data => {
+                    process.stdout.write(`stdout de ${x.clave}: ${data}`);
+                });
+                child.stderr.on('data', data => {
+                    process.stderr.write(`error de ${x.clave}: ${data}`);
+                });
+                child.on('close', code => {
+                    console.log(`cerrando svn ${x.clave}: ${code}`);
+                });
+                return promiseFromChildProcess(child);
+            } else {
+                return null;
+            }
+        });
+    }
+    await Promise.all(promesasSVN);
+    console.log("obtencion de sources completa");
+
+    /* compilar limitadon a porcesos en paralelo */
+    let promesasCompilar = [];
+    const parallel = 4;
+    let limit = pLimit(parallel);
+    promesasCompilar = sitios.sitios.map(async x => {
+        return limit(() => {
+            if (x.compilar) {
+                let child;
+                let net;
+                net = x.versionNet === 4 ? sitios.vars.net4 : sitios.vars.net2;
+                if (x.es64bit) {
+                    net = net.replace("Microsoft.NET\\Framework\\", "Microsoft.NET\\Framework64\\")
+                }
+                child = exec(
+                    `${net}\\aspnet_compiler.exe -v /${x.folder} -p svn\\${x.folder} -f built\\${x.folder}${x.xclude
+                        ? (typeof (x.xclude) === "string" ?
+                            " -x " + x.xclude :
+                            x.xclude.map(xi => " -x " + xi).join(" "))
+                        : ""}`
+                );
+                child.stdout.on('data', data => {
+                    process.stdout.write(`stdout de ${x.clave}: ${data}`);
+                });
+                child.stderr.on('data', data => {
+                    process.stderr.write(`error de ${x.clave}: ${data}`);
+                });
+                child.on('close', code => {
+                    console.log(
+                        `cerrando aspnet_compiler de ${x.clave}: ${code}`
+                    );
+                });
+                return promiseFromChildProcess(child);
+            } else {
+                return null;
+            }
+        });
+    });
+    await Promise.all(promesasCompilar);
+    console.log("Compilacion completa");
+
+    //limpiar basura y pdbs
+    console.log("limpiando datos basura");
+    const promesasLimpiaCopia = [rimraf("built/** /*.pdb"), rimraf("built/** /*.inc"), rimraf("built/** /*.log"), rimraf(
+        "built/** /*.psd")];
+    if (sitios.vars.borrarwc) {
+        promesasLimpiaCopia.push(rimraf("built/** /web.config"));
+    }
+    promesasLimpiaCopia.push(...sitios.sitios.map(x => {
+        if (x.compilar && x.postcopy) {
+            if (typeof x.postcopy === "string") {
+                return fs.copy(`svn\\${x.folder}\\${x.postcopy}`,
+                    `built\\${x.folder}\\${x.postcopy}`, {
+                    preserveTimestamps: true
+                });
+            } else {
+                return x.postcopy.map(xc => fs.copy(
+                    `svn\\${x.folder}\\${xc}`,
+                    `built\\${x.folder}\\${xc}`, {
+                    preserveTimestamps: true
+                }));
+            }
+        } else {
+            return null;
+        }
+    }));
+    await Promise.all(promesasLimpiaCopia);
+
+    //comprimir con 7z
+    if (sitios.vars.comprimir) {
+        console.log("Comprimiendo Archivos. puede tomar algun tiempo...");
+        let pass = sitios.vars.pw7;
+        pass = (pass || (`${pass}`).trim() !== "") ? (`${pass}`).trim() : false;
+        const child = exec(
+            `"${sitios.vars.z7}" a sitios.7z -mx=3 -y -ms=100m built publicados.json Microsoft.Web.Infrastructure.dll Oracle.ManagedDataAccess.dll Newtonsoft.Json.dll thePublisher.exe thePublisher.exe.config ${(pass !== false)
+                ? " -p"
+                : ""}`
+        );
+        child.stdout.pipe(process.stdout);
+        child.stderr.pipe(process.stderr);
+        if (pass !== false) {
+            child.stdin.write(`${pass}\n`);
+        }
+        child.on('close', code => {
+            console.log(`cerrando 7z: ${code}`);
+        });
+        await promiseFromChildProcess(child);
+    }
+})();
