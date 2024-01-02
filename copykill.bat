f38cfe0f1cba40422b47e5ce18821bd43744b4a0
Raul Salitrero <raulsalitrero1984@gmail.com>
2024-01-02 15:15:02 -0600

    Actualizar
    - Se quita el 211
    - Se modifican sitios
    - Ultimas versión de los dll del publisher

--- copykill.bat
+++ copykill.bat
@@ -2,8 +2,8 @@ copy /y sitios.7z \\10.18.226.137\C$\users\raul.salitrero\Desktop
 call rimraf \\10.18.226.137\C$\users\raul.salitrero\Desktop\sitios
 copy /y sitios.7z \\10.18.226.171\C$\users\raul.salitrero\Desktop
 call rimraf \\10.18.226.171\C$\users\raul.salitrero\Desktop\sitios
-copy /y sitios.7z \\10.18.226.211\C$\users\raul.salitrero\Desktop
-call rimraf \\10.18.226.211\C$\users\raul.salitrero\Desktop\sitios
+rem copy /y sitios.7z \\10.18.226.211\C$\users\raul.salitrero\Desktop
+rem call rimraf \\10.18.226.211\C$\users\raul.salitrero\Desktop\sitios
 copy /y sitios.7z \\10.18.226.212\C$\users\raul.salitrero\Desktop
 call rimraf \\10.18.226.212\C$\users\raul.salitrero\Desktop\sitios
 copy /y sitios.7z \\10.18.226.213\C$\users\raul.salitrero\Desktop
@@ -13,7 +13,8 @@ rem call rimraf \\10.18.226.200\C$\users\raul.salitrero\Desktop\sitios
 
 start mstsc /v:10.18.226.137 /admin
 start mstsc /v:10.18.226.171 /admin
-start mstsc /v:10.18.226.211 /admin
+rem el 163 no cepta copy automatico copiar archivos a mano
+start mstsc /v:10.18.226.163 /admin
 start mstsc /v:10.18.226.212 /admin
 start mstsc /v:10.18.226.213 /admin
 
